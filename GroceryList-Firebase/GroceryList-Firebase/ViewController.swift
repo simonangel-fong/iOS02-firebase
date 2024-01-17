680fe0d30f016c75ff2b300edba1c44af76f3ba7
simon <simonangelfong@gmail.com>
2024-01-17 14:26:26 -0500

    Add ios project

---
+++ GroceryList-Firebase/GroceryList-Firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroceryList-Firebase
+//
+//  Created by Simon Fong on 17/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
